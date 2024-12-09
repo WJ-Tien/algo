968d04da149fc676b4e3175ca9436d5823ffe068
WJ-Tien <chc61394@gmail.com>
2024-12-09 22:40:30 +0800

    binary search

--- leetcode_common_patterns/binary_search_algos.py
+++ leetcode_common_patterns/binary_search_algos.py
@@ -12,6 +12,8 @@ def findClosestElements(arr: list[int], k: int, x: int) -> list[int]:
 
     當我們移動到 left = mid + 1 時，我們是確定當前的 mid 不可能是答案，因為我們已經看到了比它更遠的那個點（arr[mid + k]）
     當我們設置 right = mid 時，我們需要保留 mid 這個點，因為我們還沒有確定地看到它是不是最佳起始點
+    數字正常分布之下，理論上left < x < right (也就是sliding window包含x)
+    但如果某一遍數字特大 可能sliding window都在某一側
 
     """
 
@@ -31,3 +33,29 @@ def findClosestElements(arr: list[int], k: int, x: int) -> list[int]:
             right = mid
             
     return arr[left:left + k]
+
+    
+def searchMatrix(matrix: list[list[int]], target: int) -> bool:
+
+    # key is to flatten all the indices
+    # high = len(matrix) * len(matrix[0]) - 1
+    # row = mid // cols
+    # col = mid % cols
+
+    low, high = 0, len(matrix) * len(matrix[0]) - 1
+    cols = len(matrix[0])
+
+    while low <= high:
+        mid = low + (high - low) // 2
+        row = mid // cols
+        col = mid % cols
+        if matrix[row][col] == target:
+            return True
+        elif matrix[row][col] > target:
+            high = mid - 1 
+        else:
+            low = mid + 1
+    return False
+
+
+
