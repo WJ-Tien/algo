6dfad6cb2ed165166b8fe3ce57e583ddf2b583a2
WJ-Tien <chc61394@gmail.com>
2024-12-13 22:34:24 +0800

    update

--- leetcode_common_patterns/special.py
+++ leetcode_common_patterns/special.py
@@ -43,3 +43,19 @@ def productExceptSelf(nums: list[int]) -> list[int]:
         rprod *= nums[i]
 
     return ret
+
+
+def romanToInt(s: str) -> int:
+
+    mp = {"I": 1, "V": 5, "X": 10,
+            "L": 50, "C": 100, "D": 500, 
+            "M": 1000 }
+    
+    ans = 0
+
+    for i in range(len(s) - 1):
+        if mp[s[i+1]] > mp[s[i]]:
+            ans -= mp[s[i]]
+        else:
+            ans += mp[s[i]]
+    return ans + mp[s[-1]]
