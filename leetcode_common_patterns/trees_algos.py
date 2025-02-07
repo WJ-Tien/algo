9264ffe71425f053c91824778eb27992f8e26313
WJ-Tien <chc61394@gmail.com>
2025-02-07 19:25:40 +0800

    segment tree is easier than BIT

--- leetcode_common_patterns/trees_algos.py
+++ leetcode_common_patterns/trees_algos.py
@@ -703,6 +703,8 @@ def removeLeafNodes(root: Optional[TreeNode], target: int) -> Optional[TreeNode]
 
 
 class NumArray_BIT:
+    # BIT[x] = [x, x - lowbit(x) + 1]
+    # 307. Range Sum Query - Mutable
     # T: O(N) (cons: O(n); update/query: O(log(n)))
     # S: O(N)
 
@@ -716,12 +718,14 @@ class NumArray_BIT:
         # self.BIT = [0] * (self.size + 1)
         # for i in range(self.size):
         #     self._construct(i+1, self.nums[i])
+        # O(N) approach
         for i in range(len(self.BIT)):
             next_node_idx = i + (i & -i)
             if next_node_idx < len(self.BIT):
                 self.BIT[next_node_idx] += self.BIT[i]
 
     def _construct(self, index: int, val: int) -> None:
+        # O(NlogN) to construct all slower approach
         # because we are at 0, no need to calc diff
         # get next: +
         idx = index
@@ -753,3 +757,67 @@ class NumArray_BIT:
         # prefix sum
         # 1-indexed, so we add 1 for left & right
         return self.query(right+1) - self.query(left - 1 + 1)
+    
+    
+class NumArray_SEGMENT:
+
+    def __init__(self, nums: list[int]):
+        # T: average O(logN); build_tree O(N)
+        # S: O(4*N) = O(N)
+        # h = log_2(N)
+        # number of nodes = 2^(h+1) - 1
+        # 4N can guarantee enough capacity
+        self.n = len(nums)
+        self.tree = [0] * (4 * self.n) # 4N
+        self.build_tree(nums, 0, 0, self.n-1)
+        
+    def build_tree(self, arr, node, start , end):
+        # T: O(n)
+        # node (idx) -> from tree
+        # start/end (idx) -> from arr
+        # build prefix_sum tree
+        if start == end:
+            self.tree[node] = arr[start]
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            self.build_tree(arr, left_node, start, mid)
+            self.build_tree(arr, right_node, mid+1, end)
+            self.tree[node] = self.tree[left_node] + self.tree[right_node]
+
+    def _update(self, node, start, end, index, val):
+        # T: O(logN), worst O(N)
+        if start == end:
+            self.tree[node] = val
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            if index <= mid:
+                self._update(left_node, start, mid, index, val)
+            else:
+                self._update(right_node, mid+1, end, index, val)
+            self.tree[node] = self.tree[left_node] + self.tree[right_node] 
+
+    def update(self, index: int, val: int) -> None:
+        self._update(0, 0, self.n-1, index, val)
+    
+    def _query(self, node, start, end, L, R):
+        # T: O(logN)
+        # L...R start...end L...R
+        if R < start or L > end:
+            return 0
+        elif L <= start and end <= R:
+            return self.tree[node]
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            left_sum = self._query(left_node, start, mid, L, R)
+            right_sum = self._query(right_node, mid + 1, end, L, R)
+            return left_sum + right_sum
+
+    def sumRange(self, left: int, right: int) -> int:
+        # T: O(logN)
+        return self._query(0, 0, self.n - 1, left, right)
