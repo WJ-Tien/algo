d34e732f48ca4681f13b31bbaed38d001bfd825f
WJ-Tien <chc61394@gmail.com>
2025-02-08 19:02:02 +0800

    update

--- tree_basic.py
+++ tree_basic.py
@@ -118,6 +118,78 @@ class FenwickTree:
 
 # print(fenwick.query(5))  # 查詢前 5 個元素的總和，輸出 12 (5 + 7)
 # print(fenwick.range_query(3, 7))  # 查詢索引 3 到 7 的總和，輸出 22
+# for non-complete graph we can set i-j (not exist) to +inf
+
+class SegmentTree:
+    """
+        在 Segment Tree 中，我們根據 數組的索引範圍 劃分：
+        左子樹存儲 左半區間 的資訊。
+        右子樹存儲 右半區間 的資訊。 
+        不斷切半 直到剩下一個
+    
+    """
+
+    def __init__(self, nums: list[int]):
+        # T: average O(logN); build_tree O(N)
+        # S: O(4*N) = O(N)
+        # h = log_2(N)
+        # number of nodes = 2^(h+1) - 1
+        # 4N can guarantee enough capacity
+        self.n = len(nums)
+        self.tree = [0] * (4 * self.n) # 4N
+        self.build_tree(nums, 0, 0, self.n-1)
+        
+    def build_tree(self, arr, node, start , end):
+        # T: O(n)
+        # node (idx) -> from tree
+        # start/end (idx) -> from arr
+        # build prefix_sum tree
+        if start == end:
+            self.tree[node] = arr[start]
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            self.build_tree(arr, left_node, start, mid)
+            self.build_tree(arr, right_node, mid+1, end)
+            self.tree[node] = self.tree[left_node] + self.tree[right_node]
+
+    def _update(self, node, start, end, index, val):
+        # T: O(logN), worst O(N)
+        if start == end:
+            self.tree[node] = val
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            if index <= mid:
+                self._update(left_node, start, mid, index, val)
+            else:
+                self._update(right_node, mid+1, end, index, val)
+            self.tree[node] = self.tree[left_node] + self.tree[right_node] 
+
+    def update(self, index: int, val: int) -> None:
+        self._update(0, 0, self.n-1, index, val)
+    
+    def _query(self, node, start, end, L, R):
+        # T: O(logN)
+        # L...R start...end L...R
+        if R < start or L > end:
+            return 0
+        elif L <= start and end <= R:
+            return self.tree[node]
+        else:
+            mid = start + (end - start) // 2
+            left_node = 2 * node + 1
+            right_node = 2 * node + 2
+            left_sum = self._query(left_node, start, mid, L, R)
+            right_sum = self._query(right_node, mid + 1, end, L, R)
+            return left_sum + right_sum
+
+    def sumRange(self, left: int, right: int) -> int:
+        # T: O(logN)
+        return self._query(0, 0, self.n - 1, left, right)
+
 
 
 
