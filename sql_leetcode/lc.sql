fa5b4899528b6f30805ff7a51964895f342363d0
WJ-Tien <chc61394@gmail.com>
2025-02-12 13:24:00 +0800

    sql

--- sql_leetcode/lc.sql
+++ sql_leetcode/lc.sql
@@ -1,19 +1,42 @@
 "
+normal func
+agg func
+window func
+
 agg func --> NULL WON'T be included, but count will
 primary: unique + non-null
 
 是的，b.bonus IS NULL 這個條件是 必要的，
 因為在 SQL 中，NULL 不是數字，也不能用來比較大小，所以 b.bonus < 1000 不會包含 NULL 值。
-View: select only
-SP: with input/out parameters
-M-View: cache-like
 
 self join --> need on (emp vs mgr)
 cross join --> A rows * B rows (different table)
+full (outer) join -> all rows, no matter matched or not.
+
+INSERT INTO --> need both tables exist
+SELECT INTO --> create a new table from the existing one
+
 rank: 1 1 3
 dense_rank: 1 1 2 2 3
 row_number: 1 2 3 4 
 over (partition something orderby another)
+--> rank, dense_rank, row_number, range, lead, lag
+
+view -> virutual table --> select only -> update when querying 
+materialized view (cache like) -> with real data -> need to update manually
+store procedure (SP): sort of like function in SQL, can reduce IO/CPU workload
+# IN(read), OUT(return the output), INOUT (best, bidirection)
+
+ACID
+Atomic: all successful or all failed
+Consistency: e.g., bank balance must >= 0
+isolation: each transaction is independant from each other
+durability: store the data forever 
+
+INDEX: B+ index (data distriubtion)
+Partition (same table, e.g. hash partition)
+Sharding (tables in different physical storage)
+Clustering: store similar data in the neighboring physical address
 
 "
 --197. Rising Temperature
