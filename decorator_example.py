dc65dca8b0a66929d39486a581a2dbc5d5aa7f12
WJ-Tien <chc61394@gmail.com>
2025-02-14 13:08:12 +0800

    decorator

--- decorator_example.py
+++ decorator_example.py
@@ -3,14 +3,14 @@ def deprecated(reason="TEST"):
     def fancy(func):
         def wrapper(*args, **kwargs):
             print("I am a wrapper")
+            print("Reason: ", reason)
             func(*args, **kwargs)
         return wrapper
     return fancy
 
-@deprecated
+@deprecated()
 def exile(num):
     print(f"I am an exilar: {num}")
-
 
 def countdown(n):
     while n > 0:
@@ -18,11 +18,12 @@ def countdown(n):
         print(f"back:{n}")
         n -= 1
 
-c = countdown(10)
-next(c)
-next(c)
+# c = countdown(10)
+# next(c)
+# next(c)
 
 # base_kcal = 1600 
 # print("Carbo: ", base_kcal * 0.60 / 4)
 # print("Protein: ", base_kcal * 0.15 / 4)
 # print("Fat: ", base_kcal * 0.25 / 9)
+exile(10)
